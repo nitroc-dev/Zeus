90191d57ddfafb47c903453b34bacc34e32733fb
Corentin <contact@nitroc.xyz>
2026-05-22 21:14:49 +0200

    fix: redirects

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,6 +3,6 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "fr"],
   defaultLocale: "en",
-  localePrefix: "always",
+  localePrefix: "as-needed",
   localeDetection: true,
 });
